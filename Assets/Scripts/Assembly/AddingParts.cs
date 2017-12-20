9b2b93adc3415880b159b29af0f1f6e15ac7c5e2
charlotteAuger <auger.cha@gmail.com>
2017-12-20 13:33:07 +0100

    Stitching script working

---
+++ Assets/Scripts/Assembly/AddingParts.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddingParts : MonoBehaviour {
+	public GameObject objPlayer;
+	public GameObject objLeg;
+	public GameObject objHead;
+	// Use this for initialization
+	void Start () 
+	{
+		AddLimb( objHead, objPlayer );
+		AddLimb (objLeg, objPlayer);
+	}
+
+	public void AddLimb( GameObject BonedObj, GameObject RootObj)
+	{
+		SkinnedMeshRenderer[] BonedObjects = BonedObj.gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+		foreach (SkinnedMeshRenderer SkinnedRenderer in BonedObjects)
+			ProcessBonedObject( SkinnedRenderer, RootObj );
+		Destroy (BonedObj);
+	}
+
+	void ProcessBonedObject (SkinnedMeshRenderer ThisRenderer, GameObject RootObj)
+	{
+		{
+			/*      Create the SubObject        */
+			GameObject NewObj = new GameObject( transform.gameObject.name );
+			NewObj.transform.parent = RootObj.transform;
+			/*      Add the renderer        */
+			NewObj.AddComponent<SkinnedMeshRenderer>();
+			SkinnedMeshRenderer NewRenderer = NewObj.GetComponent<SkinnedMeshRenderer>();
+			/*      Assemble Bone Structure     */
+			Transform[] MyBones = new Transform[ ThisRenderer.bones.Length ];
+			for ( int i=0; i< ThisRenderer.bones.Length ; i++ )
+				MyBones[ i ] = FindChildByName( ThisRenderer.bones[ i ].name, RootObj.transform );
+			/*      Assemble Renderer       */
+			NewRenderer.bones = MyBones;
+			NewRenderer.sharedMesh = ThisRenderer.sharedMesh;
+			NewRenderer.materials = ThisRenderer.materials;
+		}
+	}
+
+	Transform FindChildByName( string ThisName, Transform ThisGObj)
+	{
+		Transform ReturnObj;
+		if( ThisGObj.name==ThisName )
+			return ThisGObj.transform;
+		foreach (Transform child in ThisGObj)
+		{
+			ReturnObj = FindChildByName( ThisName, child );
+			if( ReturnObj )
+				return ReturnObj;
+		}
+		return null;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
